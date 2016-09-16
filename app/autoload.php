1c00ea3291f47d94c8e172306e0a770008c77dab
sciurolocutus <bob@erigad.Home>
2016-09-16 16:19:01 -0500

    Moved the javascript in the default template to the head. Rephrased the autoloader's path from __DIR__ . '/../[...]' to dirname(__DIR__) . '/[...]' Changed the secret in parameters.yml.dist

--- app/autoload.php
+++ app/autoload.php
@@ -4,7 +4,7 @@ use Doctrine\Common\Annotations\AnnotationRegistry;
 use Composer\Autoload\ClassLoader;
 
 /** @var ClassLoader $loader */
-$loader = require __DIR__.'/../vendor/autoload.php';
+$loader = require dirname(__DIR__).'/vendor/autoload.php';
 
 AnnotationRegistry::registerLoader([$loader, 'loadClass']);
 
